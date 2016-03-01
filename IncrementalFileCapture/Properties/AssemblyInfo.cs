e0e6078e3e12d75cfffe7e459a229c7fb2407eac
Andrew Ferguson <fergicide@gmail.com>
2016-03-01 20:34:26 +0800

    Update properties for publishing

--- IncrementalFileCapture/Properties/AssemblyInfo.cs
+++ IncrementalFileCapture/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IncrementalFileCapture")]
+[assembly: AssemblyTitle("Incremental File Capture")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IncrementalFileCapture")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Andrew Ferguson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
